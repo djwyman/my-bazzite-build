07453ff09d32efcbc0629fcd60d383308b1d0e31
djwyman <42300171+djwyman@users.noreply.github.com>
2024-09-13 17:22:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install screen
-rpm-ostree install kpatience
+rpm-ostree install kpat
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
